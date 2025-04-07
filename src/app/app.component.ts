2e6bd4513df0e2e7d8459c117d468d2736d016c8
Tarvo S <Zethion1@gmail.com>
2025-04-07 20:06:58 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [MatSlideToggleModule,
+    MatButton, CommonModule, RouterLink, RouterOutlet, MatToolbar, MatIcon, MatIconButton, MatMenuItem, MatMenu, MatMenuTrigger, MatDivider],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'kumnevoistlus-frontend';
 }
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {CommonModule} from '@angular/common';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {MatIcon} from '@angular/material/icon';
+import {MatDivider} from '@angular/material/divider';;
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {CompetitorComponent} from './competitor/competitor.component';
+import {ResultComponent} from './result/result.component';
+import {HomepageComponent} from './homepage/homepage.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {AdminComponent} from './admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: HomepageComponent},
+  {path: "admin/manage-competitors", component: CompetitorComponent},
+  {path: "admin/manage-results", component: ResultComponent},
+  {path: "admin", component: AdminComponent},
+  {path: "**", component: NotFoundComponent},
+];
